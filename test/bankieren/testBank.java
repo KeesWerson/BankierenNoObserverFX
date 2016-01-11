ee0c8753ab998e5296c20f9dd9d94a7ce9cd1cbe
Jim Sanders <j.sanders@student.fontys.nl>
2016-01-11 13:44:21 +0100

    yolo

--- test/bankieren/testBank.java
+++ test/bankieren/testBank.java
@@ -1,7 +1,20 @@
 package bankieren;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Kees on 11/01/2016.
  */
 public class testBank {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testOpenRekening(){
+        
+    }
 }

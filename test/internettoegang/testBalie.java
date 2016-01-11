0e19ff5739c16ac813805125140d4da1eafc188e
Werson,Kees K <k.werson@student.fontys.nl>
2016-01-11 13:51:02 +0100

    testBalie & testBankiersessie

--- test/internettoegang/testBalie.java
+++ test/internettoegang/testBalie.java
@@ -1,7 +1,46 @@
 package internettoegang;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Kees on 11/01/2016.
  */
 public class testBalie {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testOpenRekening(){
+        /**
+         * creatie van een nieuwe bankrekening; het gegenereerde bankrekeningnummer is
+         * identificerend voor de nieuwe bankrekening en heeft een saldo van 0 euro
+         * @param naam van de eigenaar van de nieuwe bankrekening
+         * @param plaats de woonplaats van de eigenaar van de nieuwe bankrekening
+         * @param wachtwoord van het account waarmee er toegang kan worden verkregen
+         * tot de nieuwe bankrekening
+         * @return null zodra naam of plaats een lege string of wachtwoord minder dan
+         * vier of meer dan acht karakters lang is en anders de gegenereerde
+         * accountnaam(8 karakters lang) waarmee er toegang tot de nieuwe bankrekening
+         * kan worden verkregen
+         */
+
+    }
+
+    @Test
+    public void testLogIn(){
+        /**
+         * er wordt een sessie opgestart voor het login-account met de naam
+         * accountnaam mits het wachtwoord correct is
+         * @param accountnaam
+         * @param wachtwoord
+         * @return de gegenereerde sessie waarbinnen de gebruiker
+         * toegang krijgt tot de bankrekening die hoort bij het betreffende login-
+         * account mits accountnaam en wachtwoord matchen, anders null
+         */
+
+    }
 }
--- test/internettoegang/testBankiersessie.java
+++ test/internettoegang/testBankiersessie.java
@@ -1,7 +1,68 @@
 package internettoegang;
 
+import fontys.util.InvalidSessionException;
+import fontys.util.NumberDoesntExistException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.rmi.RemoteException;
+
 /**
  * Created by Kees on 11/01/2016.
  */
 public class testBankiersessie {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testIsGeldig(){
+        /**
+         * @returns true als de laatste aanroep van getRekening of maakOver voor deze
+         *          sessie minder dan GELDIGHEIDSDUUR geleden is
+         *          en er geen communicatiestoornis in de tussentijd is opgetreden,
+         *          anders false
+         */
+
+    }
+
+    @Test
+    public void testMaakOver(){
+        /**
+         * er wordt bedrag overgemaakt van de bankrekening met het nummer bron naar
+         * de bankrekening met nummer bestemming
+         *
+         * @param bron
+         * @param bestemming
+         *            is ongelijk aan rekeningnummer van deze bankiersessie
+         * @param bedrag
+         *            is groter dan 0
+         * @return <b>true</b> als de overmaking is gelukt, anders <b>false</b>
+         * @throws NumberDoesntExistException
+         *             als bestemming onbekend is
+         * @throws InvalidSessionException
+         *             als sessie niet meer geldig is
+         */
+    }
+
+    @Test
+    public void testlogUit(){
+        /**
+         * sessie wordt beeindigd
+         */
+
+    }
+
+    @Test
+    public void testGetRekening(){
+        /**
+         * @return de rekeninggegevens die horen bij deze sessie
+         * @throws InvalidSessionException
+         *             als de sessieId niet geldig of verlopen is
+         * @throws RemoteException
+         */
+
+    }
 }

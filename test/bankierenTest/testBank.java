882ef13e2fcde40acb014d48360ea851ecbb780d
Jim Sanders <j.sanders@student.fontys.nl>
2016-01-12 10:09:00 +0100

    Bank tests

--- test/bankierenTest/testBank.java
+++ test/bankierenTest/testBank.java
@@ -4,6 +4,7 @@ import bank.bankieren.Bank;
 import bank.bankieren.Klant;
 import bank.bankieren.Money;
 import bank.bankieren.Rekening;
+import fontys.util.NumberDoesntExistException;
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
@@ -20,6 +21,8 @@ public class testBank extends TestCase {
     @Before
     public void setUp() throws Exception {
         bank = new Bank("Rabobank");
+        bank.openRekening("Jim Sanders","Nuth");
+        bank.openRekening("Kees Werson","Valkenswaard");
 
     }
 
@@ -38,9 +41,15 @@ public class testBank extends TestCase {
          *         gecreeerde bankrekening
          */
 
-        Assert.assertEquals("Rekening niet goed aangemaakt.",100000000,bank.openRekening("Hans Worst","Darmstadt"));
-        Assert.assertEquals("Rekening niet goed aangemaakt.", 100000001, bank.openRekening("Henk Penck", "Swekdorp"));
+        Assert.assertEquals("Rekening niet goed aangemaakt.",100000002,bank.openRekening("Hans Worst","Darmstadt"));
+
+        Klant klant = (Klant)bank.getRekening(100000002).getEigenaar();
+        Assert.assertEquals("Klant niet goed aangemaakt", "Hans Worst", klant.getNaam());
+        Assert.assertEquals("Klant niet goed aangemaakt", "Darmstadt", klant.getPlaats());
+
         Assert.assertEquals("Rekening toch aangemaakt.", -1, bank.openRekening("",""));
+        Assert.assertEquals("Rekening toch aangemaakt.", -1, bank.openRekening("Piet",""));
+        Assert.assertEquals("Rekening toch aangemaakt.", -1, bank.openRekening("","Dorp"));
     }
 
     @Test
@@ -56,8 +65,8 @@ public class testBank extends TestCase {
          *         gecreeerde bankrekening
          */
 
-        Assert.assertEquals("Rekening niet goed aangemaakt.",100000000,bank.openRekening("Hans Worst","Darmstadt"));
-        Assert.assertEquals("Rekening niet goed aangemaakt.", 100000001, bank.openRekening("Hans Worst","Darmstadt"));
+        Assert.assertEquals("Rekening niet goed aangemaakt.",100000002,bank.openRekening("Hans Worst","Darmstadt"));
+        Assert.assertEquals("Rekening niet goed aangemaakt.", 100000003, bank.openRekening("Hans Worst","Darmstadt"));
 
     }
 
@@ -65,7 +74,26 @@ public class testBank extends TestCase {
 
 
     @Test
-    public void testMaakOver(){
+    public void testMaakOver() throws NumberDoesntExistException {
+        /**
+         * er wordt bedrag overgemaakt van de bankrekening met nummer bron naar de
+         * bankrekening met nummer bestemming.
+         *
+         * @param bron
+         * @param bestemming
+         *            ongelijk aan bron
+         * @param bedrag
+         *            is groter dan 0
+         * @return <b>true</b> als de overmaking is gelukt, anders <b>false</b>
+         * @throws NumberDoesntExistException
+         *             als een van de twee bankrekeningnummers onbekend is
+         */
+
+        boolean gelukt = bank.maakOver(100000000,100000001,new Money(1000,"€"));
+
+        Assert.assertEquals("Geld niet goed bijgeschreven.",1000,bank.getRekening(100000001).getSaldo().getCents());
+        Assert.assertEquals("Geld niet goed afgeschreven.",-1000,bank.getRekening(100000000).getSaldo().getCents());
+        Assert.assertEquals("False teruggegeven.", true, gelukt);
 
     }
 
